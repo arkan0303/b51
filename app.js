0f66ef0d44486b6104440eb61d8d6f0633512aba
arkan0303 <arkanuladelis8@gmail.com>
2023-11-22 15:43:29 +0700

    update bootsrap

--- app.js
+++ app.js
@@ -1,12 +1,40 @@
 const express = require("express");
 const path = require("path");
+const { title } = require("process");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("haiiii");
-});
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "src/views"));
 
+// set stastic file server
+app.use(express.static("src/assets"));
+
+// parsing data form client
+app.use(express.urlencoded({ extended: false }));
+
+// routing
+app.get("/", (req, res) => {
+  res.render("index");
+});
+app.get("/testimonial", (req, res) => {
+  res.render("testimonial");
+});
+app.get("/contact", (req, res) => {
+  res.render("contact");
+});
+app.get("/detail-project", (req, res) => {
+  res.render("detail");
+});
+app.get("/add-project", (req, res) => {
+  res.render("project");
+});
+app.post("/add-project", (req, res) => {
+  const { title, startDate, endDate, Description, Tecnhologies } = req.body;
+
+  console.log(req.body);
+  res.redirect("/");
+});
 app.listen(port, () => {
   console.log(`server running on port ${port}`);
 });
